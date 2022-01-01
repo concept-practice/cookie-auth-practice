63540f44d2dc0b6a01491305252593aed6f5f9eb
Michael Bradvica <mjbradvica@protonmail.com>
2021-12-31 21:09:52 -0500

    project setup

---
+++ client/src/app/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App: React.FC = () => {
+	return (
+		<React.Fragment>
+			<div>Hello!</div>
+			<table className="pure-table">
+				<thead>
+					<tr>
+						<th>Title</th>
+					</tr>
+				</thead>
+			</table>
+		</React.Fragment>
+	);
+};
+
+export default App;
